fb9354444dfb7953f3f5d782d126abef57c7e816
Sindre Ilebekk Johansen <sindre@sindrejohansen.no>
2019-05-25 20:08:42 +0200

    Cargo fix

--- src/never_body.rs
+++ src/never_body.rs
@@ -1,16 +1,12 @@
 /// Returns the the headers quickly, but then never returns any body
-use std::collections::VecDeque;
 use std::net::SocketAddr;
-use std::time::{Duration, Instant};
 
-use futures::compat::Future01CompatExt;
 use futures::prelude::*;
 use futures::stream::unfold;
 use hyper::rt::Future;
 use hyper::service::service_fn;
 use hyper::{Body, Response, Server};
 use snafu::Snafu;
-use tokio::timer::Delay;
 
 #[derive(Debug, Snafu)]
 enum Error {}
@@ -28,7 +24,7 @@ pub fn bind(addr: impl Into<SocketAddr>) -> impl Future<Item = (), Error = ()> {
 async fn response() -> Result<Response<Body>, Error> {
     let stream = unfold(
         (),
-        async move |mut data| -> Option<(Result<Vec<u8>, Error>, ())> {
+        async move |_data| -> Option<(Result<Vec<u8>, Error>, ())> {
             let () = future::empty().await;
             Some((Ok(vec![12, 13, 14]), ()))
         },
